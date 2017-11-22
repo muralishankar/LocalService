7e49cf0cafe0bfe3fa69cee61d4eb4c75c458f61
ROOT\muralishankar.sundar <muralishankar.sundaram@syncfusion.com>
2017-11-22 10:08:32 +0530

    main changes
    changes for configs.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200);
     res.end('ok');
 });
-server.listen(8080);
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
